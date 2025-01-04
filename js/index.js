768e5ee07c57a9bbed065c404c6b043acda382f8
Yaroslav <lgalahad88@gmail.com>
2025-01-04 14:31:25 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function createSum() {
+    let acc = 0; 
+
+    return function(x) {
+        acc += x;
+        return acc;
+    };
+}
+
+const sum = createSum();
+
+console.log(sum(4));  // 4
+console.log(sum(6));  // 10
+console.log(sum(10)); // 20
+console.log(sum(7));  // 27
+
